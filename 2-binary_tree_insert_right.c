82223686c0bb93ef6153966cd244acc58548037b
Millennial-Polymath <muchamingotho@gmail.com>
2021-07-21 12:48:51 +0000

    fix documentation

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -4,7 +4,7 @@
  * binary_tree_insert_right - inserts a node as the right-child of another node
  * @parent: a pointer to the node to insert the right child in
  * @value: value to store in the new node
- *
+ * Return: the pointer to the new Node or None
  */
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
