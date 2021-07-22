831ace58fc481ca5079b7a700de6b9df6132fb48
Millennial-Polymath <muchamingotho@gmail.com>
2021-07-22 21:11:31 +0000

    fix task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 /**
-* binary_tree_depth - function that measures the depth of a node in a   binary tree
+* binary_tree_depth - function that
+* measures the depth of a node in a   binary tree
 * @tree: is a pointer to the node to measure the depth
 * Return: return the depth of a node, and 0 when the tree is NULL
 */
