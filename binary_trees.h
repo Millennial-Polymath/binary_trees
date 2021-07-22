48d85d1493dbcc2804ba2f3c89c1fa13fb5b9347
Millennial-Polymath <muchamingotho@gmail.com>
2021-07-22 21:05:51 +0000

    update header file

--- binary_trees.h
+++ binary_trees.h
@@ -36,4 +36,5 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 size_t binary_tree_balance(const binary_tree_t *tree);
 size_t binary_tree_is_full(const binary_tree_t *tree);
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 #endif /* BINARY_TREES_H_ */
