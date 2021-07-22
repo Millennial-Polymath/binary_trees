28bf6ed0f343ef1ed2abd41902e0e13428f3c1bd
Millennial-Polymath <muchamingotho@gmail.com>
2021-07-22 21:55:12 +0000

    add task 14 and 15

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,27 @@
 #include <stdlib.h>
 #include "binary_trees.h"
+
+/**
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
+ * Return: The height of the tree and NULL when the tree is NULL
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree == NULL)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+
+	if (left >= right)
+		return (left + 1);
+	else
+		return (right + 1);
+}
+
 /**
 * binary_tree_balance - measures the number
 * of nodes with at least 1 child of a binary tree
--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,5 +1,22 @@
 #include <stdlib.h>
 #include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - Checks if a node is a leaf
+ * @node: the pointer to the node to check
+ *
+ * Return: 1 if the node is a leaf, otherwise 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+	if (!node->left && !node->right)
+		return (1);
+	else
+		return (0);
+}
+
 /**
 * binary_tree_is_full - measures the number
 * of nodes with at least 1 child of a binary tree
@@ -11,13 +28,10 @@ size_t binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-
 	if (binary_tree_is_leaf(tree->left) && binary_tree_is_leaf(tree->right))
-	    return (1);
+		return (1);
 	else
 		return (0);
 	binary_tree_is_full(tree->left);
 	binary_tree_is_full(tree->right);
-
-
 }
