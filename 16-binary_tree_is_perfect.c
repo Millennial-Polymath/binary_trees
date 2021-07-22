bd5b6a081cae98477089bcfe3c07dee39478b12a
Millennial-Polymath <muchamingotho@gmail.com>
2021-07-22 23:01:47 +0000

    fix tsk 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -67,10 +67,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (binary_tree_is_leaf(tree))
 		return (1);
-	if (!binary_tree_is_leaf(tree))
-	{
-		if (binary_tree_balance(tree) == 0)
-			return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
-	}
+
+	if (binary_tree_balance(tree) == 0)
+		return (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right));
 	return (0);
 }
